6038284d51df0d7feed4d19d4df18fa781004f6f
Losyash-89 <chetireit@yandex.ru>
2023-10-08 22:06:34 +0300

    08.10 22:05 It is a training too

--- AndroidProject/app/src/main/java/mai/team2/lab_1/MainActivity.java
+++ AndroidProject/app/src/main/java/mai/team2/lab_1/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    int x =7;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
