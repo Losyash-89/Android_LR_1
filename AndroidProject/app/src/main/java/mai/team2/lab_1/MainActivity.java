85d5d32cda5117c9c59ccfdf9d393e13ae5d3de5
Losyash-89 <chetireit@yandex.ru>
2023-10-09 00:10:23 +0300

    23.59 Create activity "BullsAndCows"

--- AndroidProject/app/src/main/java/mai/team2/lab_1/MainActivity.java
+++ AndroidProject/app/src/main/java/mai/team2/lab_1/MainActivity.java
@@ -2,18 +2,27 @@ package mai.team2.lab_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    int x =7;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ButtonIntentToBullsAndCows = (Button) findViewById(R.id.buttonStartGame);
+        ButtonIntentToBullsAndCows.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intenToBullsAndCows = new Intent(MainActivity.this, BullsAndCowsActivity.class);
+                startActivity(intenToBullsAndCows);
+            }
+        });
     }
 
 
